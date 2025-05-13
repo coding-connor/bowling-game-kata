869cab97110d67bd4ce39b63c34c4455d9b916fb
Connor Haines <connor_haines@outlook.com>
2025-05-13 12:01:19 -0600

    initial state

--- src/bowling_game_kata/main.py
+++ src/bowling_game_kata/main.py
@@ -1,11 +1,6 @@
-from abc import ABC, abstractmethod
-from pydantic import BaseModel, field_validator
-from pydantic.dataclasses import dataclass
-
-
 def hello_world():
     return "hello world"
 
 
 if __name__ == "__main__":
-    test = hello_world()
+    print(hello_world())
